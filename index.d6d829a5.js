a366b2da4940c60d8bbb5aaff6c3809025a2abac
Ihor Kuchin <vedunigor@gmail.com>
2022-09-25 02:02:54 +0300

    Updates

---
+++ index.d6d829a5.js
@@ -0,0 +1,2 @@
+var t;t="Let's start the new project",document.querySelector("#start").textContent=t;
+//# sourceMappingURL=index.d6d829a5.js.map
---
+++ index.fbd104ad.js
@@ -0,0 +1,2 @@
+!function(){var t;t="Let's start the new project",document.querySelector("#start").textContent=t}();
+//# sourceMappingURL=index.fbd104ad.js.map
